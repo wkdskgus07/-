cf46fb506e175b9ef3b2c18a145786455bef471b
wkdskgus07 <jangnahyeon12@gmail.com>
2024-08-21 16:40:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.title("Hello")
 st.header("안녕~!")
 st.write("안녕하세요 반가워요!")
 st.subheader("Welcome to nahyeon's world!!")
-clicked = st.button("시작하기")
+clicked = st.button("게임을 시작하지")
 if clicked :
-  st.write("버튼 클릭됨")
+  st.write("사망")
