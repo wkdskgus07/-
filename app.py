99a944f5195e18f35e3fcb372b0537fa5d69c86a
wkdskgus07 <jangnahyeon12@gmail.com>
2024-08-28 16:04:21 +0900

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Hello")
-st.header("안녕~!")
-st.write("안녕하세요 반가워요!")
-st.subheader("Welcome to nahyeon's world!!")
-clicked = st.button("게임을 시작하지")
-if clicked :
-  st.write("사망")
