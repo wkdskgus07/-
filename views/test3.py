42869db3713f40a66718ae3b32b7a28cb85a5551
wkdskgus07 <jangnahyeon12@gmail.com>
2024-09-20 16:44:55 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -3,8 +3,8 @@ import random
 
 #게임 결과 내는 함수. guessNum은 사람 추측값 0(앞면), 1(뒷면)
 def playGame(guessNum):
-comNum = random.randint(0,1)
-if comNum == guessNum:
+ comNum = random.randint(0,1)
+ if comNum == guessNum:
   st.write('적중!!')
 else:
   st.write('아쉽네요. 틀렸습니다.')
