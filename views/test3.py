f832e62e72ed4b6dd9fe23191afe809d28a2f8b9
wkdskgus07 <jangnahyeon12@gmail.com>
2024-09-25 16:18:33 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -5,9 +5,9 @@ import random
 def playGame(guessNum):
  comNum = random.randint(0,1)
  if comNum == guessNum:
-  st.write('정답ㅊㅋ')
+  st.write('정답이다.')
  else:
-  st.write('풉ㅋ 이걸 틀리네')
+  st.write('풉ㅋ 이걸 틀리네. 이제 넌 살아서 나갈 수 없다.')
 
 st.title("동전 던지기 게임")
 st.divider()
@@ -15,11 +15,22 @@ st.divider()
 st.image('assets/coin_head.png')
 st.image('assets/coin_tail.png')
 
-st.header('동전 던지기 게임에 오신 것을 환영합니다.')
-st.subheader('앞면일까요? 뒷면일까요?')
+st.subheader('앞면과 뒷면 중 고르지 않는다면 넌 살아서 나갈 수 없다.')
 
 
 if st.button('앞면'):
   playGame(0)
 if st.button('뒷면'):
  playGame(1)
+
+
+col1, col2 = st.columns(2)
+col1.write('동전 앞면')
+col2.write('동전 뒷면')
+
+with col1:
+    st.image('assets/coin_head.png')
+with col2:
+    st.image('assets/coin_tail.png')
+
+st.header("동전 던지기 게임에 온 걸 환영한다. 게임을 시작하지.")
